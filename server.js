c5633f80d5785f12bea4ff223a79bebae9a5e556
Boo90 <219544365+Boo90@users.noreply.github.com>
2025-08-31 18:39:57 +0400

    Final code fix for server.js

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(express.json());
 app.use('/api', apiRoutes);
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log(err));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
